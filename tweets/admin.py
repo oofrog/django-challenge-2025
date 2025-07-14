41bca0183b19ee8befde2e1801b0aa2831b2540d
oofrog <78217274+oofrog@users.noreply.github.com>
2025-07-14 18:13:43 +0900

    Update Tweet Admin
    Tweet 어드민 패널에 Tweet의 Like 숫자를 보여주는 칼럼을 추가

--- tweets/admin.py
+++ tweets/admin.py
@@ -8,9 +8,13 @@ class TweetAdmin(admin.ModelAdmin):
     list_display = (
         "user",
         "payload",
+        "total_likes",
         "created_at",
         "updated_at",
     )
+
+    def total_likes(self,tweet):
+        return tweet.likes.count()
 
 
 @admin.register(Like)
--- tweets/models.py
+++ tweets/models.py
@@ -11,19 +11,24 @@ class Tweet(CommonModel):
     user = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
+        related_name="tweets",
     )
+
     def __str__(self):
-        return f'{self.user} / {self.payload}'
+        return f"{self.user} / {self.payload}"
 
 
 class Like(CommonModel):
     user = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
+        related_name="likes",
     )
     tweet = models.ForeignKey(
         "tweets.Tweet",
         on_delete=models.CASCADE,
+        related_name="likes",
     )
+
     def __str__(self):
-        return f'{self.user} / {self.tweet}'
+        return f"{self.user} / {self.tweet}"
