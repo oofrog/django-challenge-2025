c1ef05a2d02090e4edc46fe9b55d7de5f2ad64f2
oofrog <78217274+oofrog@users.noreply.github.com>
2025-07-24 22:21:07 +0900

    feat(users): 로그인/아웃, 비밀번호변경 API

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from .models import User
 from .serializers import TinyUserSerializer, PublicUserSerializer
 from tweets.models import Tweet
@@ -35,6 +38,23 @@ class Users(APIView):
         )
         return Response(serializer.data)
 
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PublicUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = PublicUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
 
 class UserDetail(APIView):
 
@@ -48,3 +68,49 @@ class UserDetail(APIView):
         user = self.get_object(pk)
         serializer = PublicUserSerializer(user)
         return Response(serializer.data)
+
+
+class LogIn(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "hello"})
+        else:
+            return Response({"error": "wrong password"})
+
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye"})
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_pw = request.data.get("old_password")
+        new_pw = request.data.get("new_password")
+        if not old_pw or not new_pw:
+            raise ParseError
+        if user.check_password(old_pw):
+            user.set_password(new_pw)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
